16d7b03f0b5ce0c3146558bc2a86508214af6409
Devashish <devashish.pnd18@gmail.com>
2021-11-25 12:54:46 +0530

    Chapter 2: Creating User Controls - Adding Header.

--- UsingWebControls/UsingWebControls/Default.aspx.designer.cs
+++ UsingWebControls/UsingWebControls/Default.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace UsingWebControls
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Header1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::UsingWebControls.UserControls.Header Header1;
+
+        /// <summary>
         /// FirstNameTextBox control.
         /// </summary>
         /// <remarks>
